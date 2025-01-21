80a4fe5d02525e417c978f052a73145ccbf4b831
Rafael <57674238+Luckymeme@users.noreply.github.com>
2025-01-21 20:01:59 +0100

    2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,25 +1,15 @@
-import { Routes } from '@angular/router';
-import { InicioComponent } from './inicio/inicio.component';
 import { NgModule } from '@angular/core';
+import { Routes } from '@angular/router';
+// Importamos las rutas de los componentes
+import { InicioComponent } from './inicio/inicio.component';
 import { LandingComponent } from './landing/landing.component';
 
-
-
-@NgModule({
-
-    declarations:[
-        InicioComponent,
-        LandingComponent
-    ]
-    // imports:[
-
-    // ]
-})
-
 export const routes: Routes = [
-    { path: 'inicio', component: InicioComponent },
-    { path: '', component: LandingComponent },
+  { path: 'inicio', component: InicioComponent },
+  { path: 'landing', component: LandingComponent },
+  { path: '**', component: NgModule },
+//   {
+//     path: '**',
+//     redirectTo: 'landingPage',
+//   },
 ];
-
-
-// export const routes: Routes = []
